1263fc02057dac18ee0ead14671dad80cbbcc40e
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-07 23:08:40 +0300

    Add Routes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,14 +1,25 @@
-
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import  Profile  from "./pages/Profile";
 
 function App() {
  
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </>
+    <BrowserRouter>
+ <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/profile" element={<Profile/>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+ </Routes>
+
+
+    </BrowserRouter>
   )
 }
 
---
+++ FrontEnd/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+
+
+const Login = () => {
+  return (
+    <div>SignIn</div>
+  )
+}
+
+export default Login
