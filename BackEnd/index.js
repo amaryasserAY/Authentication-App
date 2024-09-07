1cfa89ae3198fe9d10c91103ef0ffd569c724d26
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-08 00:34:33 +0300

    Create DataBass Mongo And Connect

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+/****  Global Importing And Using in Any Work  ********/
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("Connect to Mongo Successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 const PORT = 3000;
