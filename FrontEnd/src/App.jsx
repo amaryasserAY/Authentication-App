5f44f87772b2f5fcb51d5796943fa979bd72a3ac
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-07 23:33:48 +0300

    Create Heeader Component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,12 +4,15 @@ import About from "./pages/About";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import  Profile  from "./pages/Profile";
+import Header from "./components/Header";
 
 function App() {
  
 
   return (
     <BrowserRouter>
+    {/****** Header ****/}
+    <Header  />
  <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
---
+++ FrontEnd/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <div className="bg-slate-200">
+<div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <Link to="/">
+    <h1 className="font-bold">Auth App</h1>
+    </Link>
+    <ul className="flex gap-4 items-center">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/login">Log In</Link>
+    </ul>
+</div>
+    </div>
+  )
+}
+
+export default Header
