3b7750d92a147edbe7eb18627e1705361f125876
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-08 21:59:46 +0300

    complete signup

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 /****  Global Importing And Using in Any Work  ********/
-
 dotenv.config();
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_DB)
@@ -15,11 +20,19 @@ mongoose
     console.log(err);
   });
 
-const app = express();
-const PORT = 3000;
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Welcome");
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  const message = err.message || "internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
 
 app.listen(PORT, () => {
--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,8 +1,22 @@
-
-
+import {Link} from 'react-router-dom'
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className="p-3 max-w-lg mx-auto">
+<h1 className="text-3xl text-center font-semibold my-7" >Register Up</h1>
+
+
+<form className="flex flex-col gap-4">
+<input   type="text" placeholder="Username" id="username" className="bg-slate-100 p-3 rounded-lg" />
+<input   type="email" placeholder="Email" id="email" className="bg-slate-100 p-3 rounded-lg" />
+<input   type="password" placeholder="Password" id="password" className="bg-slate-100 p-3 rounded-lg" />
+<button type="submit" className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Register</button>
+</form>
+
+<div className="flex gap-2 mt-5">
+  <p>Have an Account?</p>
+  <Link className="text-blue-500" to="/login">Sign in</Link>
+</div>
+    </div>
   )
 }
 
