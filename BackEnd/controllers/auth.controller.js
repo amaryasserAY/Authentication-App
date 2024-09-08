370de1bc96740d5f6b6161518ce57e0c93653e4e
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-08 23:53:02 +0300

    Create BackEnd Login

--- BackEnd/controllers/auth.controller.js
+++ BackEnd/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,6 +14,28 @@ export const signup = async (req, res, next) => {
     await newUser.save();
 
     res.status(201).json({ message: "User saved successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "wrong credentials"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, " wrong credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+    const expiryDate = new Date(Date.now() + 3600000); // 1 hour
+    res
+      .cookie("access_token", token, { httpOnly: true, expiry: expiryDate })
+      .status(200)
+      .json(rest);
   } catch (err) {
     next(err);
   }
--- BackEnd/routes/auth.route.js
+++ BackEnd/routes/auth.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 const router = express.Router();
-import { signup } from "../controllers/auth.controller.js";
+import { login, signup } from "../controllers/auth.controller.js";
 
 router.post("/signup", signup);
-
+router.post("/login", login);
 export default router;
