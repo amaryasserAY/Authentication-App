92ceeb1b26f6b330cc3682480e8b800f3bd6f942
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-10 09:28:06 +0300

    complete The Sign in page Functionality

--- FrontEnd/src/pages/Login.jsx
+++ FrontEnd/src/pages/Login.jsx
@@ -1,8 +1,69 @@
+import { useState } from 'react';
+import {Link ,useNavigate} from 'react-router-dom'
 
 
 const Login = () => {
+
+
+  const [formData, setFormData] = useState({})
+  const [error,setError] = useState(false)
+  const [loading,setLoading] = useState(false)
+  const navigate = useNavigate();
+
+const handelChange = (e)=>{
+setFormData({...formData , [e.target.id] : e.target.value});
+}
+
+
+const handelSubmit = async (e)=>{
+  e.preventDefault() ;
+try{
+  setError(false)
+  setLoading(true);
+  const res = await fetch('/api/auth/login',{
+    method: 'POST',
+    headers:{
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData),
+  })
+  const data = await res.json()
+  setLoading(false)
+
+  if (data.success === false) {
+    setError(true) 
+  } 
+ navigate('/');
+}catch(error){
+setLoading(false)
+setError(error.message)
+}
+
+ 
+
+
+}
+
   return (
-    <div>SignIn</div>
+    <div className="p-3 max-w-lg mx-auto">
+<h1 className="text-3xl text-center font-semibold my-7" >Log In</h1>
+
+
+<form className="flex flex-col gap-4" onSubmit = {handelSubmit}>
+
+<input   type="email" placeholder="Email" id="email" className="bg-slate-100 p-3 rounded-lg "  onChange={handelChange}/>
+<input   type="password" placeholder="Password" id="password" className="bg-slate-100 p-3 rounded-lg " onChange={handelChange} />
+<button disabled={loading} type="submit" className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+  {loading ? 'Loading...' : 'Login'}
+</button>
+</form>
+
+<div className="flex gap-2 mt-5">
+  <p>Dont Have an Account?</p>
+  <Link className="text-blue-500" to="/register">Register Up</Link>
+</div>
+<p className='text-red-700 mt-5'>{error && 'something is wrong'}</p>
+    </div>
   )
 }
 
--- FrontEnd/src/pages/Register.jsx
+++ FrontEnd/src/pages/Register.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 const Register = () => {
 
 
   const [formData, setFormData] = useState({})
   const [error,setError] = useState(false)
   const [loading,setLoading] = useState(false)
+  const navigate = useNavigate();
 
 const handelChange = (e)=>{
 setFormData({...formData , [e.target.id] : e.target.value});
@@ -31,6 +32,7 @@ try{
     setError(true) 
   } 
  
+  navigate("/login")
 }catch(error){
 setLoading(false)
 setError(error.message)
