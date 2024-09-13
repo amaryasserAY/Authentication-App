39272ba3251b231ce94547f4eff7ddf23b40d88b
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-13 11:22:42 +0300

    deploy Project

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -5,14 +5,21 @@ import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 /****  Global Importing And Using in Any Work  ********/
 dotenv.config();
 const app = express();
 const PORT = 3000;
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "/FrontEnd/dist")));
+
+app.get("*", (res, req) => {
+  res.sendFile(path.join(__dirname, "FrontEnd", "dist", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGO_DB)
