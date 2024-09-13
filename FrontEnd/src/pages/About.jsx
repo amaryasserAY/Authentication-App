85fb4a31f0d2c055d864c99dd8f800b0b1279257
amaryasserAY <tokyocode.ay@gmail.com>
2024-09-13 11:07:12 +0300

    Complete Home And About Page

--- FrontEnd/src/pages/About.jsx
+++ FrontEnd/src/pages/About.jsx
@@ -2,7 +2,12 @@
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 text-center">Welcome To About Page!</h1>
+      <p className="text-slate-700 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, itaque magni, amet ipsam in, porro totam placeat accusamus labore laudantium blanditiis ipsum quis non dolorem dicta! Unde beatae reiciendis voluptatem?</p>
+      <p className="text-slate-700 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, itaque magni, amet ipsam in, porro totam placeat accusamus labore laudantium blanditiis ipsum quis non dolorem dicta! Unde beatae reiciendis voluptatem?</p>
+      <p className="text-slate-700 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, itaque magni, amet ipsam in, porro totam placeat accusamus labore laudantium blanditiis ipsum quis non dolorem dicta! Unde beatae reiciendis voluptatem?</p>
+    </div>
   )
 }
 
